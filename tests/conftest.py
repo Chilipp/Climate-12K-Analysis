5ceba75ddb448e5463c0695f73dc48b684a19ff0
Chilipp <p.sommer@posteo.de>
2019-08-07 01:37:15 +0200

    added more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ import glob
 import lipd
 from collections import OrderedDict, defaultdict
 import pandas as pd
+from functools import partial
 import numpy as np
 import re
 
@@ -33,22 +34,41 @@ all_reports = defaultdict(dict)
 fixtures_re = re.compile(r'\[.+\]')
 
 
+accepted_failures = []
+
+
 def _data_files(dir=data_dir):
     return glob.glob(osp.join(data_dir, '*.lpd'))
 
 
 def pytest_addoption(parser):
     group = parser.getgroup('LiPD')
-    group.addoption('--lipd-data', default=data_dir,
-                    help="Directory of the LiPD files to test")
+    group.addoption('--lipd-data', default=data_dir, metavar="DIRECTORY",
+                    help=("Directory of the LiPD files to test. "
+                          "Default: %(default)s"))
+    group.addoption(
+        '--accepted', default=[], type=partial(np.loadtxt, dtype=str, ndmin=1),
+        metavar="FILEPATH",
+        help=("Text file with accepted failures. Each line in this text file "
+              "must correspond to one nodeid in the tests (see the nodeid-* "
+              "columns in results.xlsx)."))
 
 
 def pytest_configure(config):
     global data_dir
     data_dir = osp.abspath(config.option.lipd_data)
+    accepted_failures.extend(config.option.accepted)
 
 
 def read_lipds(data_dir):
+    try:
+        if not series:
+            raise ValueError("Not yet defined")
+    except (NameError, ValueError):
+        _read_lipds(data_dir)
+
+
+def _read_lipds(data_dir):
     global series, lipds
     with remember_cwd():
         os.chdir(data_dir)
@@ -81,22 +101,25 @@ def pytest_generate_tests(metafunc):
             'lipd_file', files, indirect=True,
             ids=[osp.splitext(osp.basename(f))[0] for f in files])
     if 'lipd_data' in metafunc.fixturenames:
-        try:
-            if not lipds:
-                raise ValueError("Not yet defined")
-        except (NameError, ValueError):
-            read_lipds(metafunc.config.option.lipd_data)
+        read_lipds(metafunc.config.option.lipd_data)
         return metafunc.parametrize('lipd_data', list(lipds.values()),
                                     ids=list(lipds.keys()), indirect=True)
     if 'series_data' in metafunc.fixturenames:
-        try:
-            if not series:
-                raise ValueError("Not yet defined")
-        except (NameError, ValueError):
-            read_lipds(metafunc.config.option.lipd_data)
+        read_lipds(metafunc.config.option.lipd_data)
         ids = list(map(lambda d: '{dataSetName}.{paleoData_TSid}'.format(**d),
                        series))
         return metafunc.parametrize('series_data', series, ids=ids,
+                                    indirect=True)
+
+    if 'series_data_country' in metafunc.fixturenames:
+        read_lipds(metafunc.config.option.lipd_data)
+        try:
+            inject_countries(series)
+        except ImportError:
+            pass
+        ids = list(map(lambda d: '{dataSetName}.{paleoData_TSid}'.format(**d),
+                       series))
+        return metafunc.parametrize('series_data_country', series, ids=ids,
                                     indirect=True)
 
 
@@ -109,7 +132,9 @@ def pytest_runtest_makereport(item, call):
     # we only look at actual failing test calls, not setup/teardown
     props = dict(rep.user_properties)
     if rep.when == "call" and 'dataSetName' in props:
-        props[fixtures_re.sub('', rep.nodeid)] = rep.outcome
+        funcname = fixtures_re.sub('', rep.nodeid)
+        props[funcname] = rep.outcome
+        props['nodeid-' + funcname] = rep.nodeid
         all_reports[props['dataSetName']].update(props)
 
 
@@ -119,46 +144,71 @@ def pytest_sessionfinish(session):
     df.index.names = ['dataSetName', 'TSid']
 
     general = df.loc[df.index.get_level_values(1).isnull()]
-    TS_specific = df.loc[df.index.get_level_values(1).notnull()].reset_index(
-        level=1).copy(True)
+    TS_specific = df.loc[df.index.get_level_values(1).notnull()]
 
-    general.index = general.index.droplevel(1)
-    del general['dataSetName']
+    if len(general):
 
-    cols = general.columns[general.notnull().any()]
+        TS_specific = TS_specific.reset_index(level=1).copy(True)
 
-    general = general.loc[np.unique(TS_specific.index)]
+        general.index = general.index.droplevel(1)
+        del general['dataSetName']
 
-    TS_specific.loc[general.index, cols] = general[cols]
-    TS_specific.set_index('TSid', append=True, inplace=True)
+        cols = general.columns[general.notnull().any()]
+
+        general = general.loc[np.unique(TS_specific.index)]
+
+        TS_specific.loc[general.index, cols] = general[cols]
+        TS_specific.set_index('TSid', append=True, inplace=True)
 
     TS_specific.to_excel(osp.join(data_dir, 'results.xlsx'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def lipd_data(request):
     """The dictionary of a LiPD file"""
     return request.param
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def series_data(request):
     """The dictionary of a time series"""
     return request.param
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def pd_series(series_data):
     """A pandas series of the data"""
     meta_cols = ['geo_meanLon', 'geo_meanLat', 'dataSetName',
                  'paleoData_TSid', 'paleoData_variableName']
-    return pd.Series(
+    ret = pd.Series(
         np.asarray(series_data['paleoData_values'], dtype=float),
         index=np.asarray(series_data['age'], dtype=float),
         name=tuple(series_data.get(name, np.nan) for name in meta_cols))
+    return ret[ret.index.notnull()]
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def lipd_file(request):
     """The path to a lipd file"""
     return request.param
+
+
+@pytest.fixture(scope='session')
+def series_data_country(request):
+    return request.param
+
+
+@pytest.fixture
+def accepted(request):
+    return request.node._nodeid in accepted_failures
+
+
+def inject_countries(series):
+    from latlon_utils import get_country_gpd
+    lon = np.array([d['geo_meanLon'] for d in series])
+    lat = np.array([d['geo_meanLat'] for d in series])
+
+    countries = get_country_gpd(lat, lon)
+
+    for country, d in zip(countries, series):
+        d['geo_natEarth'] = country
--- tests/test_data.py
+++ tests/test_data.py
@@ -5,6 +5,7 @@ import os
 import os.path as osp
 import contextlib
 import numpy as np
+import pytest
 
 
 @contextlib.contextmanager
@@ -18,7 +19,7 @@ def remember_cwd():
         os.chdir(cwd)
 
 
-def test_lipd_validity(lipd_file, tmp_path, record_property):
+def test_lipd_validity(lipd_file, tmp_path, record_property, accepted):
     """Test to load the lipd files"""
     record_property('dataSetName',
                     (osp.splitext(osp.basename(lipd_file))[0], None))
@@ -27,11 +28,11 @@ def test_lipd_validity(lipd_file, tmp_path, record_property):
     shutil.copyfile(lipd_file, str(tmp_path / base))
     with remember_cwd():
         os.chdir(str(tmp_path))
-        assert lipd.readLipd('.')
+        assert accepted or lipd.readLipd('.')
     record_property('valid file', True)
 
 
-def test_chronology_points(lipd_data, record_property):
+def test_chronology_points(lipd_data, record_property, accepted):
     """Test the existence of chronological points"""
     series = lipd.extractTs(lipd_data, 'all', 'chron')
     record_property('nchronpoints', 0)
@@ -40,48 +41,91 @@ def test_chronology_points(lipd_data, record_property):
     chronpoints = next(
         (d for d in series if d['chronData_variableName'] == 'age14C'),
         None)
-    assert chronpoints is not None, "No age14C series found!"
+    assert accepted or chronpoints is not None, "No age14C series found!"
     vals = np.array([np.nan if val == 'nan' else val
                      for val in chronpoints['age14C']], float)
-    assert len(vals) and not np.isnan(vals).all()
+    assert accepted or (len(vals) and not np.isnan(vals).all())
     record_property('nchronpoints', np.sum(~np.isnan(vals)))
 
 
-def test_temperature_values(pd_series, record_property):
+def test_temperature_values(pd_series, record_property, accepted):
     """Test the validity of the temperature values"""
     record_property('dataSetName', pd_series.name[-3:-1])
     invalid = pd_series.size - pd_series.notnull().sum()
     record_property('invalid_temperatures', invalid)
     record_property('min', pd_series.min())
     record_property('max', pd_series.max())
-    assert (pd_series > -40).all(), 'Too low temperatures!'
-    assert (pd_series < 50).all(), 'Too high temperatures!'
+    assert accepted or (pd_series > -40).all(), 'Too low temperatures!'
+    assert accepted or (pd_series < 50).all(), 'Too high temperatures!'
 
 
-def test_latlon(series_data, record_property):
+def test_latlon(series_data, record_property, accepted):
     record_property('dataSetName', (series_data['dataSetName'],
                                     series_data['paleoData_TSid']))
     lat = series_data['geo_meanLat']
     lon = series_data['geo_meanLon']
     record_property('lat_valid', lat >= -90 and lat <= 90)
     record_property('lon_valid', lon >= -180 and lon <= 360)
+    record_property(
+        'googleMapsUrl',
+        f'https://www.google.com/maps/search/?api=1&query={lat},{lon}')
 
-    assert lat >= -90 and lat <= 90, 'Invalid latitudes'
-    assert lon >= -180 and lon <= 360, 'Invalid longitudes'
+    assert accepted or lat >= -90 and lat <= 90, 'Invalid latitudes'
+    assert accepted or lon >= -180 and lon <= 360, 'Invalid longitudes'
 
 
-def test_ages(pd_series, record_property):
+def test_min_age(pd_series, record_property, accepted):
     record_property('dataSetName', pd_series.name[-3:-1])
     ages = pd_series.index.to_series()
     record_property('min_age', ages.min())
+    assert accepted or ages.min() > -70, "Invalid modern age"
+
+
+def test_max_age(pd_series, record_property, accepted):
+    record_property('dataSetName', pd_series.name[-3:-1])
+    ages = pd_series.index.to_series()
     record_property('max_age', ages.max())
-    assert ages.min() > -70, "Invalid modern age"
+    assert accepted or ages.max() < 200000, "unrealistically old age"
 
 
-def test_elev(series_data, record_property):
+def test_anomaly(pd_series, record_property, accepted):
+    """Test if a time series represents temperature anomalies"""
+    record_property('dataSetName', pd_series.name[-3:-1])
+    pd_series = pd_series[~pd_series.index.duplicated()]
+    youngest = pd_series.loc[[pd_series.idxmin()]].abs().min()
+    record_property('youngest_T_absolute', youngest)
+    assert accepted or youngest > 1e-4, "Youngest reconstruction close to 0!"
+
+
+def test_elev(series_data, record_property, accepted):
     record_property('dataSetName', (series_data['dataSetName'],
                                     series_data['paleoData_TSid']))
     elev = series_data['geo_meanElev']
     record_property('Elevation', elev)
     if 'marine' in series_data['archiveType'].lower():
-        assert elev < 0
+        assert accepted or elev < 0
+
+
+def test_duplicated_ages(pd_series, record_property, accepted):
+    record_property('dataSetName', pd_series.name[-3:-1])
+    duplicated = pd_series.index.duplicated(False).sum()
+    record_property('duplicatedAges', duplicated)
+    assert accepted or duplicated == 0
+
+
+def test_country(series_data_country, record_property, accepted):
+    data = series_data_country
+    record_property('dataSetName', (data['dataSetName'],
+                                    data['paleoData_TSid']))
+
+    record_property('natEarth_Country', data.get('geo_natEarth', ''))
+    record_property('countryOcean', data.get('geo_countryOcean', ''))
+
+    try:
+        if not data.get('geo_natEarth') or np.isnan(data['geo_natEarth']):
+            pytest.skip("No natural earth data available")
+            return
+    except TypeError:
+        pass
+    assert accepted or data['geo_natEarth'].lower() in data.get(
+        'geo_countryOcean', '').lower()
